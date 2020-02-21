50cbdd35dc221c075cfda8c8396e08a412fddd0d
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-02-21 16:09:31 +0100

    new

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Cocktail < ApplicationRecord
-  # has_one_attached :photo
+  has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
